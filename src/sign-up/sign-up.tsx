aab76eb338b84d823a17ed050a01ce64da50ffa5
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-04-29 15:10:57 -0300

    feat(sign-up): password input now switches
    between text and password types

--- src/sign-up/sign-up.tsx
+++ src/sign-up/sign-up.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { Formik, FormikProps } from 'formik';
+import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
 
 import styles from './sign-up.module.css';
 import SignUpForm from './types';
 
 export default function SignUp() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+
   const formInitialValues: SignUpForm = {
     email: '',
     username: '',
@@ -14,6 +18,25 @@ export default function SignUp() {
   const onSubmit = async (): Promise<void> => {
     Promise.resolve();
   };
+
+  const buttonHideOrShowPassword = () => (
+    <button
+      type="button"
+      onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+      aria-label={
+        isPasswordVisible ? 'Hide password' : 'Show password'
+      }
+      className={styles['password-input__button']}
+    >
+      {isPasswordVisible ? (
+        <BsEyeSlashFill
+          className={styles['password-input__svg']}
+        />
+      ) : (
+        <BsEyeFill className={styles['password-input__svg']} />
+      )}
+    </button>
+  );
 
   return (
     <div className={styles['sign-up']}>
@@ -47,32 +70,40 @@ export default function SignUp() {
               />
             </label>
 
-            <label htmlFor="password" className={styles.form__label}>
-              <div>Password</div>
-              <input
-                id="password"
-                type="password"
-                className={styles.form__input}
-                onChange={handleChange}
-                value={values.password}
-                placeholder="#super_secret123"
-              />
-            </label>
-
-            <label htmlFor="password" className={styles.form__label}>
-              <div>Password confirmation</div>
-              <input
-                id="password"
-                type="password"
-                className={styles.form__input}
-                onChange={handleChange}
-                value={values.passwordConfirmation}
-                placeholder="#super_secret123"
-              />
-            </label>
-          </form>
-        )}
-      </Formik>
+              <label htmlFor="password" className={styles.form__label}>
+                <div>Password</div>
+                <span className={styles['form__password-input']}>
+                  <input
+                    id="password"
+                    type={isPasswordVisible ? 'text' : 'password'}
+                    className={styles.form__input}
+                    onChange={handleChange}
+                    value={values.password}
+                    placeholder="#super_secret123"
+                  />
+                  {buttonHideOrShowPassword()}
+                </span>
+              </label>
+              <label
+                htmlFor="passwordConfirmation"
+                className={styles.form__label}
+              >
+                <div>Password confirmation</div>
+                <span className={styles['form__password-input']}>
+                  <input
+                    id="passwordConfirmation"
+                    type={isPasswordVisible ? 'text' : 'password'}
+                    className={styles.form__input}
+                    onChange={handleChange}
+                    value={values.passwordConfirmation}
+                    placeholder="#super_secret123"
+                  />
+                  {buttonHideOrShowPassword()}
+                </span>
+              </label>
+            </form>
+          )}
+        </Formik>
     </div>
   );
 }
