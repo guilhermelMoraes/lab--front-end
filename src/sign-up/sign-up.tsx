d41293562dd36e9bf68a7636bf1ebae0d2381689
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-05-24 10:33:02 -0300

    feat: add title to sign-up page

--- src/sign-up/sign-up.tsx
+++ src/sign-up/sign-up.tsx
@@ -1,13 +1,19 @@
+import Helmet from 'react-helmet';
 import SignUpForm from './sign-up-form';
 import styles from './sign-up.module.css';
 
 export default function SignUp() {
   return (
-    <div className={styles['sign-up']}>
-      <main className={`${styles['sign-up__form-wrapper']} container bg-body rounded shadow-lg p-3`}>
-        <h1 className="text-center mb-4">THE LAB 🧪</h1>
-        <SignUpForm />
-      </main>
-    </div>
+    <>
+      <Helmet>
+        <title>THE LAB - Sign Up</title>
+      </Helmet>
+      <div className={styles['sign-up']}>
+        <main className={`${styles['sign-up__form-wrapper']} container bg-body rounded shadow-lg p-3`}>
+          <h1 className="text-center mb-4">THE LAB 🧪</h1>
+          <SignUpForm />
+        </main>
+      </div>
+    </>
   );
 }
