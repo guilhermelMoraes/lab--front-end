2833e5021bddcb19c6ea598f40d4dfb5d55a7e86
Guilherme L. Moraes <guilherme.lmoraes.devel@gmail.com>
2022-06-19 21:32:25 -0300

    feat: add types for sign up with google

--- src/sign-up/types.ts
+++ src/sign-up/types.ts
@@ -3,16 +3,24 @@ export type FullName = {
   surname: string;
 };
 
-type LocalSignUpData = {
+export type SubmitNewUserResponse = {
+  status: 'SUCCESS' | 'ALREADY-EXIST' | 'ERROR';
+  data?: string;
+};
+
+export type GoogleSignUpData = {
+  email: string;
+  fullName: FullName;
+  emailVerified: boolean;
+};
+
+export type LocalSignUpData = {
   email: string;
   fullName: FullName;
   password: string;
   passwordConfirmation: string;
 };
 
-export type SubmitNewUserResponse = {
-  status: 'SUCCESS' | 'ALREADY-EXIST' | 'ERROR';
-  data?: string;
-};
+type UserSignUpData = LocalSignUpData | GoogleSignUpData;
 
-export default LocalSignUpData;
+export default UserSignUpData;
